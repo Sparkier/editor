946ccda4772f1754ba0e1e1e508b3aa03ab49ddf
Alex <alex@a13x.io>
2022-11-23 09:52:06 -0800

    fix: make slider update correctly when new pulse is selected

--- src/features/performance/Controls.tsx
+++ src/features/performance/Controls.tsx
@@ -37,18 +37,26 @@ interface SliderProps {
   values: Values;
   onChange: ({min, max}: {min: number; max: number}) => void;
 }
+
 const Slider: React.FC<SliderProps> = ({values, onChange}) => {
   const time = Object.entries(values).map((v) => {
     return v[1]['value'].time as number;
   });
-  const max = Math.ceil(Math.max(...time) * 100) / 100;
-  const min = Math.floor(Math.min(...time) * 100) / 100;
+  let max = Math.ceil(Math.max(...time) * 100) / 100;
+  let min = Math.floor(Math.min(...time) * 100) / 100;
   const [minVal, setMinVal] = React.useState(min);
   const [maxVal, setMaxVal] = React.useState(max);
   const minValRef = React.useRef<HTMLInputElement>(null);
   const maxValRef = React.useRef<HTMLInputElement>(null);
   const range = React.useRef<HTMLDivElement>(null);
   const getPercent = React.useCallback((value: number) => Math.round(((value - min) / (max - min)) * 100), [min, max]);
+
+  React.useEffect(() => {
+    max = Math.ceil(Math.max(...time) * 100) / 100;
+    min = Math.floor(Math.min(...time) * 100) / 100;
+    setMinVal(min);
+    setMaxVal(max);
+  }, [values]);
 
   React.useEffect(() => {
     if (maxValRef.current) {
@@ -78,41 +86,45 @@ const Slider: React.FC<SliderProps> = ({values, onChange}) => {
   }, [minVal, maxVal, onChange]);
 
   return (
-    <div className="container">
-      <input
-        type="range"
-        min={min}
-        max={max}
-        step={0.01}
-        value={minVal}
-        ref={minValRef}
-        onChange={(event) => {
-          const value = Math.min(+event.target.value, maxVal);
-          setMinVal(value);
-          event.target.value = value.toString();
-        }}
-        className="thumb thumb--zindex-3"
-      />
-      <input
-        type="range"
-        min={min}
-        max={max}
-        step={0.01}
-        value={maxVal}
-        ref={maxValRef}
-        onChange={(event) => {
-          const value = Math.max(+event.target.value, minVal);
-          setMaxVal(value);
-          event.target.value = value.toString();
-        }}
-        className="thumb thumb--zindex-4"
-      />
-      <div className="slider">
-        <div className="slider__track" />
-        <div ref={range} className="slider__range"></div>
-        <div className="slider__left-value">{minVal}</div>
-        <div className="slider__right-value">{maxVal}</div>
-      </div>
-    </div>
+    <>
+      {min !== max && (
+        <div className="container">
+          <input
+            type="range"
+            min={min}
+            max={max}
+            step={0.01}
+            value={minVal}
+            ref={minValRef}
+            onChange={(event) => {
+              const value = Math.min(+event.target.value, maxVal);
+              setMinVal(value);
+              event.target.value = value.toString();
+            }}
+            className="thumb thumb--zindex-3"
+          />
+          <input
+            type="range"
+            min={min}
+            max={max}
+            step={0.01}
+            value={maxVal}
+            ref={maxValRef}
+            onChange={(event) => {
+              const value = Math.max(+event.target.value, minVal);
+              setMaxVal(value);
+              event.target.value = value.toString();
+            }}
+            className="thumb thumb--zindex-4"
+          />
+          <div className="slider">
+            <div className="slider__track" />
+            <div ref={range} className="slider__range"></div>
+            <div className="slider__left-value">{minVal}</div>
+            <div className="slider__right-value">{maxVal}</div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
